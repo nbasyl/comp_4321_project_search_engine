fe5397f8c17ef2ab9552e381d7f5548db5f0e790
seantheplug <syliu1998@gmail.com>
2020-04-10 16:34:44 +0800

    minor changed

--- src/main/java/web/useraction/controller/WebCrawlerController.java
+++ src/main/java/web/useraction/controller/WebCrawlerController.java
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Controller;
 //import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
-import web.useraction.Crawler.Crawler;
+import webCrawler.Crawler;
 import java.util.Vector;
 
 
@@ -20,24 +20,10 @@ public class WebCrawlerController {
         try
         {
             Crawler crawler = new Crawler("http://www.cs.ust.hk/~dlee/4321/");
-
-
             Vector<String> words = crawler.extractWords();
-
-//            System.out.println("Words in "+crawler.getUrl()+":");
-//            for(int i = 0; i < words.size(); i++)
-//                System.out.print(words.get(i)+" ");
-//            System.out.println("\n\n");
-
-
             Vector<String> links = crawler.extractLinks();
-//            System.out.println("Links in "+crawler.getUrl()+":");
-//            for(int i = 0; i < links.size(); i++)
-//                System.out.println(links.get(i));
-//            System.out.println("");
             mv.addObject("words", words);
             mv.addObject("links", links);
-            System.out.println(mv);
             return mv;
 
         }
--- src/main/java/webCrawler/Crawler.java
+++ src/main/java/webCrawler/Crawler.java
@@ -1,4 +1,4 @@
-package web.useraction.Crawler;
+package webCrawler;
 
 import java.util.Vector;
 import org.htmlparser.beans.StringBean;
